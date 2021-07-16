0f527724625f06395dabfbabda9335910838e0e9
Kansioo <45654930+Kansioo@users.noreply.github.com>
2021-07-16 18:32:55 +0300

    Update CTFPlugin.kt

--- us/zonix/ctf/CTFPlugin.kt
+++ us/zonix/ctf/CTFPlugin.kt
@@ -1,8 +1,12 @@
 package us.zonix.ctf
 
 import cc.fyre.proton.Proton
+import org.bukkit.Bukkit
 import org.bukkit.plugin.java.JavaPlugin
+import us.zonix.ctf.listener.GameListener
+import us.zonix.ctf.listener.JoinListener
 import us.zonix.ctf.manager.GameManager
+import us.zonix.ctf.nametags.Nametags
 import us.zonix.ctf.scoreboard.Scoreboard
 
 class CTFPlugin : JavaPlugin() {
@@ -12,7 +16,13 @@ class CTFPlugin : JavaPlugin() {
     override fun onEnable() {
         instance = this
         Proton.getInstance().commandHandler.registerAll(this);
-        Proton.getInstance().scoreboardHandler.configuration = Scoreboard.create();
+        Proton.getInstance().scoreboardHandler.configuration = Scoreboard.create()
+
+    }
+
+    fun registerListeners() {
+        Bukkit.getPluginManager().registerEvents(GameListener(), this)
+        Bukkit.getPluginManager().registerEvents(JoinListener(), this)
     }
 
     companion object {
